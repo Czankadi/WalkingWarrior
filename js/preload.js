ef3be5b080b3c6ba36035995664ceea2810a4847
Adam <czankadi@gmail.com>
2017-09-03 17:17:26 +0200

    First uploaded version

---
+++ js/preload.js
@@ -0,0 +1,19 @@
+var Preload = function(game){};
+
+Preload.prototype = {
+
+	preload: function(){ 
+		this.game.load.image('1', 'assets/gemBlue.png');
+		this.game.load.image('2', 'assets/gemGreen.png');
+		this.game.load.image('3', 'assets/gemRed.png');
+		this.game.load.image('4', 'assets/gemYellow.png');
+		this.game.load.image('5', 'assets/gemBonusBlue.png');
+		this.game.load.image('6', 'assets/gemBonusGreen.png');
+		this.game.load.image('7', 'assets/gemBonusRed.png');
+		this.game.load.image('8', 'assets/gemBonusYellow.png');
+	},
+
+	create: function(){
+		this.game.state.start("Main");
+	}
+}
