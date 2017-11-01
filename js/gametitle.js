1b2eb3c133716b38baa452e5c905e9b4ab6461ad
Adam <czankadi@gmail.com>
2017-11-02 00:27:08 +0100

    23 Levels version 0.6

--- js/gametitle.js
+++ js/gametitle.js
@@ -11,9 +11,11 @@ GameTitle.prototype = {
         }
              background = game.add.tileSprite(0, 0, 1400, 1920, "background");
             var text = "Walking Warrior";
+             var tex3 = "v0.6";
     var style = { font: "180px Arial", fill: "#000", align: "center" };
+    var style2 = { font: "60px Arial", fill: "#000", align: "center" };
     var t = this.game.add.text(80, 100, text, style);
-    
+    var t2= this.game.add.text(1250, 1840, tex3, style2); 
     button = game.add.button(460, 350, 'playbutton', actionOnClick, this, 2, 1, 0);
    button.scale.setTo(0.91,0.91);
    
